18b70f8b77aee6e5d77a6221d2157f349fe06d83
Elliot Perreault <168759915+elliotperreault@users.noreply.github.com>
2025-03-04 16:56:36 -0500

    created my routes

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from "express";
+import defaultRoutes from "./routes/defaultRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/", defaultRoutes);
+app.use("/", adminRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
