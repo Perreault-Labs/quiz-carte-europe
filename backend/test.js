eba363cf2a30b7bb57f8c656378110a14826a53b
Elliot Perreault <168759915+elliotperreault@users.noreply.github.com>
2025-03-28 14:19:48 -0400

    created basic test.js

---
+++ backend/test.js
@@ -0,0 +1,16 @@
+import { PrismaClient } from "prisma"
+
+const prisma = new PrismaClient()
+
+
+async function main() {
+    console.log("Connected to Prisma")
+    console.log("hello")
+}
+
+main().catch(e => {
+    console.log("An error occured", e)
+}).finally(async () => {
+    await prisma.$disconnect()
+    console.log("Disconnected from Prisma")
+})
